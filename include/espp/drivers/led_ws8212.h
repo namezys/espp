29c10d359d6fbcf6b401f2974b2815615cf63967
Nick Ryzhkov <namezys@gmail.com>
2020-04-04 01:50:04 +0200

    color utilities

--- include/espp/drivers/led_ws8212.h
+++ include/espp/drivers/led_ws8212.h
@@ -10,6 +10,59 @@
 namespace espp {
 void IRAM_ATTR SendLed(uint32_t mask, uint32_t* buffer, uint32_t size);
 
+struct LedColor{
+    uint8_t green;
+    uint8_t red;
+    uint8_t blue;
+
+    LedColor():
+        green(0), red(0), blue(0)
+    {}
+
+    LedColor(uint8_t R, uint8_t G, uint8_t B):
+        green(G), red(R), blue(B)
+    {}
+
+    static
+    uint8_t _Truncate(int32_t c)
+    {
+        return std::max(std::min(c, 0xFF), 0);
+    }
+};
+
+inline
+LedColor operator*(const LedColor& color, uint32_t c)
+{
+    assert(c < 0xFFu);
+    return {
+        static_cast<uint8_t>(color.red * c / 0xFFu),
+        static_cast<uint8_t>(color.green * c / 0xFFu),
+        static_cast<uint8_t>(color.blue * c / 0xFFu)
+    };
+}
+
+inline
+LedColor operator*(uint32_t c, const LedColor& color)
+{
+    return (color * c);
+}
+
+inline
+LedColor operator+(const LedColor& color, int32_t c)
+{
+    return {
+        LedColor::_Truncate(color.red + c),
+        LedColor::_Truncate(color.green + c),
+        LedColor::_Truncate(color.blue + c),
+    };
+}
+
+inline
+LedColor operator-(const LedColor& color, int32_t c)
+{
+    return color + (-c);
+}
+
 template<std::size_t length>
 class LedStrip{
 private:
@@ -29,12 +82,6 @@ private:
     }
 
 public:
-    struct LedColor{
-        uint8_t green;
-        uint8_t red;
-        uint8_t blue;
-    };
-
     explicit
     LedStrip(GpioPin& pin): _gpio_pin(pin), _buffer(), _colors(reinterpret_cast<uint8_t*>(_buffer.data()))
     {
