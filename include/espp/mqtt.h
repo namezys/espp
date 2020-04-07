aa1e78c572c580c23458dace3da117df1428da0f
Nick Ryzhkov <namezys@gmail.com>
2020-04-08 00:00:14 +0200

    Improve Mqtt

--- include/espp/mqtt.h
+++ include/espp/mqtt.h
@@ -1,50 +1,96 @@
 #pragma once
 
-#include <string>
-#include <vector>
+#include "freertos/FreeRTOS.h"
+#include "freertos/timers.h"
 
 #include "mqtt_client.h"
 
 #include "espp/log.h"
+#include "espp/mutex.h"
+
+#include <string>
+#include <vector>
+#include <map>
 
 namespace espp {
 
-class MqTT {
+using MqttMsg = std::vector<uint8_t>;
+
+class MqttSubscription {
+public:
+    virtual void OnEvent(esp_mqtt_event_handle_t event)
+    {
+        if (event->data_len > 0) {
+            MqttMsg msg(event->data, event->data + event->data_len);
+            OnEventData(msg);
+        }
+    }
+    virtual void OnEventData(const MqttMsg& msg) = 0;
+};
+
+class Mqtt {
 public:
     explicit
-    MqTT(std::string  url);
+    Mqtt(std::string  url, std::string status_topic = {});
+    virtual ~Mqtt();
 
-    virtual ~MqTT();
+    void Init(const MqttMsg& status_msg = {});
+    void Connect();
+    bool Disconnect();
 
-    void Init();
-
-    void publish(const char* topic, const char* data, std::size_t data_len)
+    bool Publish(const char* topic, const char* data, std::size_t data_len, bool retain = false)
     {
-        assert(_client != nullptr);
+        DEBUG << "Publish message to" << topic << "retain" << retain;
         assert(data_len > 0);
-        ESP_ERROR_CHECK(esp_mqtt_client_publish(_client, topic, data, data_len, 0, 0));
+        espp::Mutex::LockGuard lock(_mutex);
+        assert(_client != nullptr);
+        return ESP_OK == esp_mqtt_client_publish(_client, topic, data, data_len, 0, retain ? 1 : 0);
     }
 
-    void publish(const std::string& topic, const std::vector<uint8_t>& data)
+    bool Publish(const std::string& topic, const MqttMsg& data, bool retain = false)
     {
-        publish(topic.data(), reinterpret_cast<const char*>(data.data()), data.size());
+        return Publish(topic.data(), reinterpret_cast<const char*>(data.data()), data.size(), retain);
+    }
+
+    void UpdateStatus(const MqttMsg& msg)
+    {
+        assert(!_status_topic.empty());
+        Publish(_status_topic, msg, true);
     }
 
 protected:
-    virtual void OnConnect(int session_present) {}
-    virtual void OnEvent(const esp_mqtt_event_handle_t& event) {}
+    virtual void OnConnect(int session_present);
+    virtual void OnDisconnect();
+    virtual void OnEvent(const esp_mqtt_event_handle_t& event);
+
+    virtual void OnStatusUpdateTimer() {}
+
+    void Subscribe(MqttSubscription& subscription, const std::string& topic)
+    {
+        assert(_subscriptions.count(topic) == 0);
+        _subscriptions[topic] = &subscription;
+    }
 
 private:
     const std::string _url;
+    const std::string _status_topic;
+    const int _keep_alive_timeout = 15;
+    const UBaseType_t _update_status_interval = pdMS_TO_TICKS(5000);
+    espp::Mutex _mutex;
     esp_mqtt_client_handle_t _client = nullptr;
+    std::map<std::string, MqttSubscription*> _subscriptions;
+    TimerHandle_t _update_status_timer = nullptr;
+    bool _is_connection = false;
 
     static
-    bool _Init(bool init);
+    esp_err_t _EventHandler(esp_mqtt_event_handle_t event);
 
     static
-    esp_err_t EventHandler(esp_mqtt_event_handle_t event);
+    void _UpdateStatusEventHandler(TimerHandle_t xTimer);
 
-    void ProcessEvent(esp_mqtt_event_handle_t event);
+    void _ProcessEvent(esp_mqtt_event_handle_t event);
+    void _ProcessConnect(int session_present);
+    void _ProcessDisconnect();
 };
 
 };
--- mqtt.cpp
+++ mqtt.cpp
@@ -1,39 +1,16 @@
 #include "freertos/FreeRTOS.h"
+#include "mqtt_client.h"
 
 #include "espp/mqtt.h"
+#include "espp/utils/macros.h"
 
 #include <utility>
-#include "mqtt_client.h"
 
 namespace espp {
 
-MqTT::MqTT(std::string url):
-    _url(std::move(url))
-{
-    assert(_Init(true));
-}
+namespace {
 
-MqTT::~MqTT()
-{
-    if (_client != nullptr) {
-        ESP_ERROR_CHECK(esp_mqtt_client_stop(_client));
-    }
-    assert(!_Init(false));
-}
-
-void MqTT::Init()
-{
-    esp_mqtt_client_config_t mqtt_cfg = {};
-    mqtt_cfg.uri = _url.data();
-    mqtt_cfg.event_handle = EventHandler;
-    mqtt_cfg.user_context = this;
-
-    esp_mqtt_client_handle_t client = esp_mqtt_client_init(&mqtt_cfg);
-    esp_mqtt_client_start(client);
-    _client = client;
-}
-
-bool MqTT::_Init(bool init)
+bool _SetMqttSingleton(bool init)
 {
     static bool is_inited = false;
     if (init == is_inited) {
@@ -43,24 +20,149 @@ bool MqTT::_Init(bool init)
     return true;
 }
 
-esp_err_t MqTT::EventHandler(esp_mqtt_event_handle_t event)
+}
+
+Mqtt::Mqtt(std::string url, std::string status_topic):
+    _url(std::move(url)), _status_topic(std::move(status_topic))
 {
-    MqTT* impl = reinterpret_cast<MqTT*>(event->user_context);
-    impl->ProcessEvent(event);
+    ESPP_CHECK(_SetMqttSingleton(true));
+}
+
+Mqtt::~Mqtt()
+{
+    ESPP_CHECK(_SetMqttSingleton(false));
+    if (_client != nullptr) {
+        ESP_ERROR_CHECK(esp_mqtt_client_destroy(_client));
+    }
+    if (_update_status_timer != nullptr) {
+        ESPP_CHECK(pdPASS == xTimerDelete(_update_status_timer, portMAX_DELAY));
+    }
+}
+
+void Mqtt::Init(const MqttMsg& status_msg)
+{
+    esp_mqtt_client_config_t mqtt_cfg = {};
+    mqtt_cfg.uri = _url.data();
+    mqtt_cfg.event_handle = _EventHandler;
+    mqtt_cfg.user_context = this;
+    if (!_status_topic.empty()) {
+        DEBUG << "Init Mqtt LWT";
+        assert(!status_msg.empty());
+        mqtt_cfg.lwt_msg = reinterpret_cast<const char*>(status_msg.data());
+        mqtt_cfg.lwt_msg_len = status_msg.size();
+        mqtt_cfg.lwt_topic = _status_topic.c_str();
+        mqtt_cfg.lwt_qos = 0;
+        mqtt_cfg.lwt_retain = 1;
+        mqtt_cfg.keepalive = _keep_alive_timeout;
+
+        DEBUG << "Init update status timer";
+        _update_status_timer = xTimerCreate(nullptr, _update_status_interval, pdTRUE,
+                                            this, _UpdateStatusEventHandler);
+        assert(_update_status_timer != nullptr);
+    }
+
+    _client = esp_mqtt_client_init(&mqtt_cfg);
+    ESPP_CHECK(_client != nullptr);
+}
+
+void Mqtt::Connect()
+{
+    INFO << "Connect Mqtt";
+    espp::Mutex::LockGuard lock(_mutex);
+    assert(!_is_connection);
+    ESP_ERROR_CHECK(esp_mqtt_client_start(_client));
+    _is_connection = true;
+}
+
+bool Mqtt::Disconnect()
+{
+    INFO << "Disconnect Mqtt";
+    espp::Mutex::LockGuard lock(_mutex);
+    if (!_is_connection) {
+        DEBUG << "No connection";
+        return false;
+    }
+    if (ESP_OK != esp_mqtt_client_stop(_client)) {
+        DEBUG << "Can't stop because mqtt was stopped";
+    }
+    _is_connection = false;
+    return true;
+}
+
+void Mqtt::OnConnect(int)
+{
+}
+
+void Mqtt::OnDisconnect()
+{
+    Disconnect();
+}
+
+void Mqtt::OnEvent(const esp_mqtt_event_handle_t& event)
+{
+    const std::string topic(event->topic, event->topic_len);
+    DEBUG << "Got message in topic" << topic;
+    const auto it = _subscriptions.find(topic);
+    if (it != _subscriptions.end()) {
+        DEBUG << "Found subscription";
+        it->second->OnEvent(event);
+    } else {
+        DEBUG << "Can't find subscription";
+    }
+}
+
+esp_err_t Mqtt::_EventHandler(esp_mqtt_event_handle_t event)
+{
+    Mqtt* impl = reinterpret_cast<Mqtt*>(event->user_context);
+    impl->_ProcessEvent(event);
     return ESP_OK;
 }
 
-void MqTT::ProcessEvent(esp_mqtt_event_handle_t event)
+void Mqtt::_UpdateStatusEventHandler(TimerHandle_t xTimer)
+{
+    Mqtt* impl = reinterpret_cast<Mqtt*>(pvTimerGetTimerID(xTimer));
+    impl->OnStatusUpdateTimer();
+}
+
+void Mqtt::_ProcessEvent(esp_mqtt_event_handle_t event)
 {
     switch(event->event_id) {
         case MQTT_EVENT_CONNECTED:
+            _ProcessConnect(event->session_present);
             OnConnect(event->session_present);
+            break;
+        case MQTT_EVENT_DISCONNECTED:
+            _ProcessDisconnect();
+            OnDisconnect();
             break;
         case MQTT_EVENT_DATA:
             OnEvent(event);
             break;
         default:
             ;
+    }
+}
+
+void Mqtt::_ProcessConnect(int session_present)
+{
+    DEBUG << "Process connection. Subscribe all subscription";
+    espp::Mutex::LockGuard lock(_mutex);
+    for(auto& subscription: _subscriptions) {
+        ESPP_CHECK(esp_mqtt_client_subscribe(_client, subscription.first.c_str(), 0) != -1);
+    }
+    if (_update_status_timer != nullptr) {
+        DEBUG << "Start update status timer";
+        ESPP_CHECK(pdPASS == xTimerStart(_update_status_timer, portMAX_DELAY));
+    }
+}
+
+void Mqtt::_ProcessDisconnect()
+{
+    DEBUG << "Process disconnect";
+    espp::Mutex::LockGuard lock(_mutex);
+    if (_update_status_timer != nullptr) {
+        DEBUG << "Stop update status timer";
+        ESPP_CHECK(pdPASS == xTimerStop(_update_status_timer, portMAX_DELAY));
     }
 }
 
