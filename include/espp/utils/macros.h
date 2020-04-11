fb0f3ce9254e725a091a0c5b60f2e0b59f1aaf5a
Nick Ryzhkov <namezys@gmail.com>
2020-04-11 20:01:22 +0200

    my assert and check macros
    Potentially assert can be disabled for speed optimization but check must always execute his instruction

--- include/espp/utils/macros.h
+++ include/espp/utils/macros.h
@@ -3,3 +3,7 @@
 #include "espp/log.h"
 
 #define ESPP_CHECK(x) if (!(x)) { ROW_LOG << "CRITICAL ASSERT AT" <<  __FILE__  << __LINE__;  std::abort(); }
+#define ESPP_ASSERT ESPP_CHECK
+
+#undef assert
+#define assert ESPP_ASSERT
