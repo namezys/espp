660a291088588e1a46432f6e9dc5291243e92e4f
Nick Ryzhkov <namezys@gmail.com>
2020-04-27 23:19:40 +0200

    nvs class wrapper

---
+++ include/espp/utils/nvs_settings.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <string>
+#include <nvs.h>
+#include <vector>
+
+namespace espp {
+
+class NvsSettings{
+public:
+    NvsSettings();
+
+    bool isInited() const
+    {
+        return _isStorageInited;
+    }
+
+    void Load();
+
+    void Save();
+
+protected:
+    virtual void DoLoad()
+    {
+    }
+
+    virtual void DoSave()
+    {
+    }
+
+    std::string ReadStr(const char* name)
+    {
+        size_t length;
+        const auto getResult = nvs_get_str(_nvs, name, nullptr, &length);
+        if(getResult == ESP_ERR_NVS_NOT_FOUND) {
+            return {};
+        }
+        ESP_ERROR_CHECK(getResult);
+        length += 1;
+        std::vector<char> result(length);
+        ESP_ERROR_CHECK(nvs_get_str(_nvs, name, result.data(), &length));
+        return {result.data(), result.data() + length};
+    }
+
+    void WriteStr(const char* name, const std::string& str)
+    {
+        ESP_ERROR_CHECK(nvs_set_str(_nvs, name, str.c_str()));
+    }
+
+private:
+    nvs_handle _nvs = {};
+    bool _isStorageInited = false;
+};
+
+}
---
+++ utils/nvs_settings.cpp
@@ -0,0 +1,48 @@
+#include <espp/utils/nvs_settings.h>
+
+#include <espp/log.h>
+#include <nvs_flash.h>
+
+namespace espp {
+
+NvsSettings::NvsSettings()
+{
+    INFO << "Init NVS";
+    ESP_ERROR_CHECK(nvs_flash_init());
+    esp_err_t ret = nvs_flash_init();
+    if(ret == ESP_ERR_NVS_NO_FREE_PAGES) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(ret);
+    if(ESP_OK != nvs_open("settings", NVS_READWRITE, &_nvs)) {
+        INFO << "Can't init settings";
+        _isStorageInited = false;
+    } else {
+        INFO << "Storage was inited";
+        _isStorageInited = true;
+        Load();
+    }
+}
+
+void NvsSettings::Load()
+{
+    if(!_isStorageInited) {
+        DEBUG << "Store not inited. Skip load";
+        return;
+    }
+    DoLoad();
+}
+
+void NvsSettings::Save()
+{
+    if(!_isStorageInited) {
+        DEBUG << "Store not inited. Skip load";
+        return;
+    }
+    DoSave();
+    ESP_ERROR_CHECK(nvs_commit(_nvs));
+}
+
+
+}
